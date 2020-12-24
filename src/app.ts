56c35be257be34834e9357414fccc0b97d9b3a66
Hajime Takeuchi <shadowthe8156@gmail.com>
2020-12-24 21:33:09 +0900

    database configをインフラ層に移動

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,7 @@
 import express, { Application, Request, Response } from 'express';
-import mysql from 'mysql';
-import { Config, config } from './config/Config';
+import { config } from './infrastructure/config/Config';
 
-const createMySqlConnection = (config: Config) => {  
-  const connection = mysql.createConnection({
-    host: config.mysql.host,
-    port: config.mysql.port,
-    user: config.mysql.user,
-    password: config.mysql.password,
-    database: config.mysql.database,
-  });
-  
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log('connected');
-  });
-
-  return connection;
-};
+import { createMySqlConnection } from './infrastructure/database/mysql';
 
 const app: Application = express();
 const con = createMySqlConnection(config);
@@ -29,7 +13,6 @@ app.get('/', (req: Request, res: Response) => {
     if (err != null) res.send(err.sqlMessage);
     res.send(results);
   });
-  //res.send('Hello!!');
 });
 
 app.listen(3000, () => console.log('server running'));
---
+++ src/infrastructure/database/mysql.ts
@@ -0,0 +1,19 @@
+import mysql, { Connection } from 'mysql';
+import { Config } from '../config/Config';
+
+export const createMySqlConnection = (config: Config): Connection => {  
+  const connection = mysql.createConnection({
+    host: config.mysql.host,
+    port: config.mysql.port,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected');
+  });
+
+  return connection;
+};
