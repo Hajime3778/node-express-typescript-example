90d4619454710c7706f52dce3b643d438ce87a95
Hajime Takeuchi <shadowthe8156@gmail.com>
2020-12-27 01:13:59 +0900

    controller 異常系テスト追加

--- test/UserController.test.ts
+++ test/UserController.test.ts
@@ -38,12 +38,16 @@ const mockUser: UserTable =
 function createMockUsecase(): IUserUsecase { 
   const mockUsecase: IUserUsecase = {
    getAll: jest.fn(() => new Promise<UserTable[]>((resolve) => resolve(mockUsers))),
-    getById: jest.fn((id: string) => new Promise<UserTable>((resolve) => {
+    getById: jest.fn((id: string) => new Promise<UserTable>((resolve, reject) => {
       if (id === '1') resolve(mockUser);
+      else reject();
     })),
-   create: jest.fn((user: User) => new Promise<string>((resolve) => resolve(user.id as string))),
-   update: jest.fn((user: User) => new Promise((resolve) => resolve(user.id))),
-   delete: jest.fn((id: string) => new Promise((resolve) => resolve(id)))
+   create: jest.fn((user: User) => new Promise<string>((resolve, reject) => user ? resolve(user.id as string): reject())),
+   update: jest.fn((user: User) => new Promise((resolve, reject) => user ? resolve(user.id): reject())),
+   delete: jest.fn((id: string) => new Promise((resolve, reject) => { 
+      if (id === '1') resolve(id);
+      else reject();
+    }))
   };
  return mockUsecase;
 }
@@ -105,5 +109,50 @@ describe('UserRepository 正常系テスト', () => {
     app.use('/api/', controller.router);
     return request(app).delete('/api/users/1')
             .expect(204);
+    });
+});
+
+describe('UserRepository 異常系テスト', () => {
+  it('getAll', () => {
+    const app: Application = express();
+    const mockUsecase = createMockUsecase();
+    mockUsecase.getAll = jest.fn(() => new Promise<UserTable[]>((resolve, reject) => reject('')));
+    const controller = new UserController(mockUsecase);
+    app.use('/api/', controller.router);
+    return request(app).get('/api/users')
+            .expect(500);
+  });
+  it('getById', () => {
+    const app: Application = express();
+    const mockUsecase = createMockUsecase();
+    const controller = new UserController(mockUsecase);
+    app.use('/api/', controller.router);
+    return request(app).get('/api/users/2')
+            .expect(500);
+  });
+  it('create', async () => {
+    const app: Application = express();
+    const mockUsecase = createMockUsecase();
+    const controller = new UserController(mockUsecase);
+
+    app.use('/api/', controller.router);
+    return request(app).post('/api/users').set('Accept', 'application/json')
+            .expect(400);
+  });
+  it('update', () => {
+    const app: Application = express();
+    const mockUsecase = createMockUsecase();
+    const controller = new UserController(mockUsecase);
+    app.use('/api/', controller.router);
+    return request(app).put('/api/users').set('Accept', 'application/json')
+            .expect(400);
+  });
+  it('delete', () => {
+    const app: Application = express();
+    const mockUsecase = createMockUsecase();
+    const controller = new UserController(mockUsecase);
+    app.use('/api/', controller.router);
+    return request(app).delete('/api/users/2')
+            .expect(500);
     });
 });
